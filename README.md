de600d04f31eee16c07d15474bfd29a593eb4d49
Kieran Hanrahan <kieran@knhn.me>
2023-04-21 13:10:33 -0400

    It work!

---
+++ README.md
@@ -0,0 +1,22 @@
+# Copy Location
+
+Plugin for [Autodesk Flame software](http://www.autodesk.com/products/flame).
+
+Copy location of selected items inside of Flame.  
+
+Example below:
+`hostname > project_name > WORKSPACE > Desktop > Reels > Reel 1 > BLACK`
+
+## Installation
+**Tested & working on 2021.1 & 2024 PR180**
+
+To make available to all users on the workstation, copy `copy_location.py` to `/opt/Autodesk/shared/python`
+
+For specific users, copy to `/opt/Autodesk/user/<user name>/python`
+
+## Menus
+- Right-click selected items on the Desktop `->` Copy... `->` Location to Clipboard
+- Right-click selected items in the Media Panel `->` Copy... `->` Location to Clipboard
+
+## Acknowledgements
+UI Templates courtesy of [pyflame.com](http://www.pyflame.com)
---
+++ copy_location.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+"""
+Copy Location
+
+URL:
+
+    http://github.com/khanrahan/copy-location
+
+Description:
+
+    Copy location of item inside of Flame (as opposed to the path in the file system).
+
+Menus:
+
+    Right-click selected items on the Desktop --> Copy... --> Copy Location
+    Right-click selected items in the Media Panel --> Copy... --> Copy Location
+
+To Install:
+
+    For all users, copy this file to:
+    /opt/Autodesk/shared/python
+
+    For a specific user, copy this file to:
+    /opt/Autodesk/user/<user name>/python
+"""
+
+from __future__ import print_function
+import socket
+import flame
+from PySide2 import QtWidgets
+
+TITLE = "Copy Location"
+VERSION_INFO = (1, 0, 0)
+VERSION = ".".join([str(num) for num in VERSION_INFO])
+TITLE_VERSION = "{} v{}".format(TITLE, VERSION)
+MESSAGE_PREFIX = "[PYTHON HOOK]"
+SEPARATOR = ' > '
+
+
+def message(string):
+    """Print message to shell window and append global MESSAGE_PREFIX."""
+
+    print(" ".join([MESSAGE_PREFIX, string]))
+
+
+def copy_to_clipboard(text):
+    """Self explanitory.  Only takes a string."""
+
+    qt_app_instance = QtWidgets.QApplication.instance()
+    qt_app_instance.clipboard().setText(text)
+
+
+def get_hostname():
+    '''Return hostname without the domain.'''
+
+    hostname = socket.gethostname().split('.')[0]
+
+    return hostname
+
+
+def find_parents(starting_item):
+    '''Returns a list of parent object names ascending from right to left.'''
+
+    current_item = starting_item
+    parents = [current_item.name.get_value()]
+
+    while current_item.parent:
+        if isinstance(current_item.parent, flame.PyProject):
+            parents.insert(0, current_item.parent.name)  # already a string
+        else:
+            parents.insert(0, current_item.parent.name.get_value())
+
+        current_item = current_item.parent
+
+    parents.insert(0, get_hostname())  # make hostname the root
+
+    return parents
+
+
+def copy_locations(selection):
+
+    message(TITLE_VERSION)
+    message("Script called from {}".format(__file__))
+
+    paths = ''
+
+    for item in selection:
+        location_path = SEPARATOR.join(find_parents(item))
+        message(location_path)
+        paths += location_path
+        paths += '\n'
+
+    copy_to_clipboard(paths)
+
+    message("Copied to clipboard!")
+    message('Done!')
+
+
+def scope_clip(selection):
+
+    for item in selection:
+        if isinstance(item, flame.PyClip):
+            return True
+    return False
+
+
+def get_media_panel_custom_ui_actions():
+
+    return [{'name': "Copy...",
+             'actions': [{'name': "Location to Clipboard",
+                          'isVisible': scope_clip,
+                          'execute': copy_locations,
+                          'minimumVersion': "2021.1"}]
+            }]
